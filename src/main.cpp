3ccf4b563a2ef861b882923a2272c01e1a4d407c
HEX9CF <hex9cf@aliyun.com>
2024-12-03 12:20:31 +0800

    Add start game menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <curses.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 
@@ -9,6 +10,7 @@ using namespace std;
 
 long delay = 100;
 
+// 球
 struct Ball {
 	int x;
 	int y;
@@ -17,15 +19,18 @@ struct Ball {
 	char str = 'O';
 } ball;
 
+// 板
 struct Bar {
 	int x;
 	int y;
-	string str = "__________";
+	string str = "[########]";
 	int length = 10;
 } bar;
 
+void startGame();
+
 // 设置定时器
-int setTicker(long n_msecs) {
+int setTimer(long n_msecs) {
 	struct itimerval new_timeset;
 	long n_sec, n_usecs;
 	n_sec = n_msecs / 1000;
@@ -37,8 +42,17 @@ int setTicker(long n_msecs) {
 	return setitimer(ITIMER_REAL, &new_timeset, NULL);
 }
 
+// 游戏结束画面
+void gameOverView() {
+	clear();
+	mvprintw(LINES / 2 - 1, COLS / 2 - 10, "Game Over!");
+	mvprintw(LINES / 2 + 1, COLS / 2 - 10, "Press 'q' to quit");
+	mvprintw(LINES / 2 + 2, COLS / 2 - 10, "Press 'r' to restart");
+	refresh();
+}
+
 // 信号处理函数
-void paint(int n) {
+void update(int n) {
 	clear();
 	mvaddch(ball.y, ball.x, ball.str);
 	mvaddstr(bar.y, bar.x, bar.str.c_str());
@@ -71,37 +85,15 @@ void paint(int n) {
 
 	// game over
 	if (ball.y >= bar.y) {
-		setTicker(0);
-		clear();
-		mvprintw(LINES / 2 - 1, COLS / 2 - 10, "Game Over!");
-		mvprintw(LINES / 2 + 1, COLS / 2 - 10, "Press 'q' to quit");
-		mvprintw(LINES / 2 + 2, COLS / 2 - 10, "Press 'r' to restart");
-		refresh();
+		setTimer(0);
+		gameOverView();
 	}
 }
 
-void init() {
-	ball.x = 5;
-	ball.y = 5;
-	ball.vx = 2;
-	ball.vy = 1;
-	bar.x = 10;
-	bar.y = LINES - 1;
-
-	// 设置定时器
-	signal(SIGALRM, paint);
-	setTicker(delay);
-}
-
-int main(int argc, char* argv[]) {
+// 游戏
+void keyboardHandler() {
 	chtype input;
 
-	// 初始化
-	initscr();
-	crmode();
-	noecho();
-
-	init();
 	while ((input = getch()) && input != ERR && input != 'q') {
 		switch (input) {
 			case 'j': {
@@ -123,11 +115,54 @@ int main(int argc, char* argv[]) {
 				break;
 			}
 			case 'r': {
-				init();
+				startGame();
 				break;
 			}
 		}
 	}
+}
+
+// 开始游戏
+void startGame() {
+	// 初始化
+	ball.x = 5;
+	ball.y = 5;
+	ball.vx = 2;
+	ball.vy = 1;
+	bar.x = 10;
+	bar.y = LINES - 1;
+
+	// 设置定时器
+	signal(SIGALRM, update);
+	setTimer(delay);
+
+	keyboardHandler();
+}
+
+// 开始画面
+void startView() {
+	chtype input;
+
+	clear();
+	mvprintw(LINES / 2 - 1, COLS / 2 - 10, "Pinball Game");
+	mvprintw(LINES / 2 + 1, COLS / 2 - 10, "Press 'q' to quit");
+	mvprintw(LINES / 2 + 2, COLS / 2 - 10, "Press 's' to start");
+	refresh();
+	while ((input = getch()) && input != ERR && input != 'q') {
+		if (input == 's') {
+			startGame();
+			break;
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	// 初始化
+	initscr();
+	crmode();
+	noecho();
+
+	startView();
 
 	endwin();
 	return 0;
