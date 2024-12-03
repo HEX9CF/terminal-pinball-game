5ae3c5f4f1c412c87d5ef4d7782e42f7d55275a2
HEX9CF <hex9cf@aliyun.com>
2024-12-03 10:07:04 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <curses.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	int x = 10;
+	int y = 10;
+	int direction = 1;
+	char ball = 'O';
+
+	// 初始化
+	initscr();
+	crmode();
+	noecho();
+
+	while (1) {
+		clear();
+		mvaddch(y, x, ball);
+		refresh();
+
+		// 更新坐标
+		x += direction;
+
+		// 变换方向
+		if (x == COLS) {
+			direction = -1;
+			x = COLS - 1;
+			beep();
+		}
+
+		if (x < 0) {
+			direction = 1;
+			x = 0;
+			beep();
+		}
+
+		usleep(100000);
+	}
+
+	endwin();
+	return 0;
+}
